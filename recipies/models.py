2efe3b01916175a69b5b6bbe8a3e301f3453126d
Sarah <sarahjbousfield@gmail.com>
2026-05-25 17:44:08 +1200

    Expanding Recipe a bit

--- recipies/models.py
+++ recipies/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='images/', null=True, default=None)
+    prep_time = models.IntegerField(default=0)
+    cook_time = models.IntegerField(default=0)
+    serves = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
