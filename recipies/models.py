1dfa16634424d33070aa298148de74ea849fc003
Sarah <sarahjbousfield@gmail.com>
2026-05-25 14:50:50 +1200

    Got distracted making things pretty

--- recipies/models.py
+++ recipies/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.ImageField(upload_to='images/', null=True, default=None)
 
     def __str__(self):
         return self.name
