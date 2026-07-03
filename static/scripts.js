35d82484a9313c34ae09b0087c70a993861ae610
Sarah <sarahjbousfield@gmail.com>
2026-07-03 15:46:20 +1200

    Keep awake WIP

--- static/scripts.js
+++ static/scripts.js
@@ -7,4 +7,72 @@ window.onload = (event) => {
     document.getElementById("close-nav").addEventListener('click', () => {
         document.querySelector('.navigation').classList.remove('open');
     });
+
+    if ('wakeLock' in navigator) {
+        let wakeLock = null;
+        document.getElementById("keep-awake-field").style.display = "block";
+
+        async function requestWakeLock() {
+          if (wakeLock !== null) return;
+
+          try {
+            wakeLock = await navigator.wakeLock.request('screen');
+
+            // Listen for the 'release' event
+            wakeLock.addEventListener('release', () => {
+                console.log('Wake Lock was released');
+            });
+            console.log('Wake Lock active');
+          } catch (err) {
+            console.error('Wake lock request failed:', err);
+          }
+        }
+
+        // Automatically release the wake lock when the page is hidden
+        document.addEventListener('visibilitychange', () => {
+            if (wakeLock !== null && document.hidden) {
+              wakeLock.release()
+                .then(() => console.log('Wake lock on page visibility change.'));
+            } else if (!document.hidden) {
+              requestWakeLock();
+            }
+        });
+
+        document.getElementById("keep-awake-field").addEventListener('change', async event => {
+            if (event.target.checked) {
+                await requestWakeLock();
+            } else if (wakeLock !== null) {
+                wakeLock.release();
+            }
+        });
+    }
 };
+
+
+
+// class WakeLockManager {
+//   #sentinel = null;
+//
+//   get isActive() {
+//     return this.#sentinel !== null && !this.#sentinel.released;
+//   }
+//
+//   async acquire() {
+//     if (this.isActive) return;
+//     try {
+//       this.#sentinel = await navigator.wakeLock.request('screen');
+//       this.#sentinel.addEventListener('release', () => {
+//         this.#sentinel = null;
+//       });
+//     } catch (err) {
+//       console.error('Failed to acquire wake lock:', err);
+//     }
+//   }
+//
+//   async release() {
+//     if (this.#sentinel) {
+//       await this.#sentinel.release();
+//       this.#sentinel = null;
+//     }
+//   }
+// }
