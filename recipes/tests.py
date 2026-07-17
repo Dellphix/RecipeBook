a71661144f5d12e57d1bf3f78e7a269797539b80
Sarah Bousfield <79132565+Dellphix@users.noreply.github.com>
2026-07-17 15:20:37 +1200

    Adding CI (#2)
    * Create django.yml
    
    Signed-off-by: Sarah Bousfield <79132565+Dellphix@users.noreply.github.com>
    
    * Update CI python version
    
    * Update CI
    
    * Fixed requirements location
    
    * env for test step
    
    * Updated tests
    
    ---------
    
    Signed-off-by: Sarah Bousfield <79132565+Dellphix@users.noreply.github.com>

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
@@ -6,12 +7,13 @@ from .models import Recipe
 
 class RecipeModelTests(TestCase):
     def test_is_viewable_by(self):
-        user = User.objects.create_user(username="testuser", password="password2026")
+        user = get_user_model().objects.create(username="testuser")
+        user.set_password('password2026')
         user.save()
         recipe = Recipe(is_public=False, user_id=user.id)
         recipe.save()
         self.assertIs(recipe.is_viewable_by(user), True)
-        user_two = User(username='two')
+        user_two = get_user_model().objects.create(username='two')
         user_two.save()
         self.assertIs(recipe.is_viewable_by(user_two), False)
         recipe.is_public = True
@@ -19,13 +21,17 @@ class RecipeModelTests(TestCase):
 
 class RecipePublicIndexViewTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username="testuser", password="password2026")
+        self.user = get_user_model().objects.create(username="testuser", password="password2026")
+        self.user.set_password('password2026')
+        self.user.save()
 
     def test_logged_out_sees_public(self):
         # setup
         recipe = Recipe(name="Test Recipe", is_public=False, user_id=self.user.id)
         recipe.save()
-        user_two = User.objects.create_user(username="testuserTwo", password="password2026")
+        user_two = get_user_model().objects.create(username="testuserTwo")
+        user_two.set_password('password2026')
+        user_two.save()
         recipe_two = Recipe(name="User Two Recipe", is_public=True, user_id=user_two.id)
         recipe_two.save()
 
@@ -45,7 +51,9 @@ class RecipePublicIndexViewTests(TestCase):
         # setup
         recipe = Recipe(name="Test Recipe", is_public=False, user_id=self.user.id)
         recipe.save()
-        user_two = User.objects.create_user(username="testuserTwo", password="password2026")
+        user_two = get_user_model().objects.create(username="testuserTwo")
+        user_two.set_password('password2026')
+        user_two.save()
         recipe_two = Recipe(name="User Two Recipe", is_public=True, user_id=user_two.id)
         recipe_two.save()
 
@@ -65,7 +73,9 @@ class RecipePublicIndexViewTests(TestCase):
 
 class RecipeIndexViewTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username="testuser", password="password2026")
+        self.user = get_user_model().objects.create(username="testuser", password="password2026")
+        self.user.set_password('password2026')
+        self.user.save()
 
     def test_see_no_user_recipes(self):
         # not logged in
@@ -90,7 +100,9 @@ class RecipeIndexViewTests(TestCase):
 
 class RecipeDetailViewTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username="testuser", password="password2026")
+        self.user = get_user_model().objects.create(username="testuser")
+        self.user.set_password('password2026')
+        self.user.save()
         self.recipe = Recipe(name="Test Recipe", is_public=False, user_id=self.user.id)
         self.recipe.save()
 
@@ -102,7 +114,8 @@ class RecipeDetailViewTests(TestCase):
         self.assertEqual(response.context["recipe"], self.recipe)
 
     def test_other_user_cant_see_recipe(self):
-        user_two = User.objects.create_user(username="testuserTwo", password="password2026")
+        user_two = get_user_model().objects.create(username="testuserTwo")
+        user_two.set_password('password2026')
         user_two.save()
         self.client.login(username="testuserTwo", password="password2026")
         response = self.client.get(reverse('recipes:detail', kwargs={'uuid': self.recipe.uuid}))
@@ -110,7 +123,7 @@ class RecipeDetailViewTests(TestCase):
 
     def test_public_cant_see_recipe(self):
         response = self.client.get(reverse('recipes:detail', kwargs={'uuid': self.recipe.uuid}))
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 302) # redirect to login
 
     def test_public_can_see_public_recipe(self):
         self.recipe.is_public = True
@@ -123,7 +136,9 @@ class RecipeDetailViewTests(TestCase):
 
 class RecipeUpdateViewTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username="testuser", password="password2026")
+        self.user = get_user_model().objects.create(username="testuser")
+        self.user.set_password('password2026')
+        self.user.save()
         self.recipe = Recipe(name="Test Recipe", is_public=False, user_id=self.user.id)
         self.recipe.save()
 
@@ -135,7 +150,8 @@ class RecipeUpdateViewTests(TestCase):
         self.assertEqual(response.context["recipe"], self.recipe)
 
     def test_other_user_cant_edit_recipe(self):
-        user_two = User.objects.create_user(username="testuserTwo", password="password2026")
+        user_two = get_user_model().objects.create(username="testuserTwo")
+        user_two.set_password('password2026')
         user_two.save()
         self.client.login(username="testuserTwo", password="password2026")
         response = self.client.get(reverse('recipes:update', kwargs={'uuid': self.recipe.uuid}))
@@ -143,4 +159,31 @@ class RecipeUpdateViewTests(TestCase):
 
     def test_public_cant_edit_recipe(self):
         response = self.client.get(reverse('recipes:update', kwargs={'uuid': self.recipe.uuid}))
+        self.assertEqual(response.status_code, 302) # Redirecto to login
+
+class RecipeDeleteViewTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(username="testuser")
+        self.user.set_password('password2026')
+        self.user.save()
+        self.recipe = Recipe(name="Test Recipe", is_public=False, user_id=self.user.id)
+        self.recipe.save()
+
+    def test_user_can_delete_recipe(self):
+        self.client.login(username="testuser", password="password2026")
+        response = self.client.get(reverse('recipes:delete', kwargs={'uuid': self.recipe.uuid}))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Delete Test Recipe')
+        self.assertEqual(response.context["recipe"], self.recipe)
+
+    def test_other_user_cant_delete_recipe(self):
+        user_two = get_user_model().objects.create(username="testuserTwo")
+        user_two.set_password('password2026')
+        user_two.save()
+        self.client.login(username="testuserTwo", password="password2026")
+        response = self.client.get(reverse('recipes:delete', kwargs={'uuid': self.recipe.uuid}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_public_cant_delete_recipe(self):
+        response = self.client.get(reverse('recipes:delete', kwargs={'uuid': self.recipe.uuid}))
         self.assertEqual(response.status_code, 302) # Redirecto to login
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db.models.sql import query
 from django.test import TestCase
@@ -11,25 +12,25 @@ class InvitationModelTests(TestCase):
         self.invitation.save()
 
     def test_is_viewable_when_unused(self):
-        response = self.client.get(reverse("invitation", kwargs={'code': self.invitation.code.hex}))
+        response = self.client.get(reverse("invitation", kwargs={'code': self.invitation.code}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "You are cordially invited to use this Recipe Book.")
 
     def test_is_not_viewable_when_used(self):
-        user = User()
+        user = get_user_model().objects.create()
         user.save()
         self.invitation.user_id = user.id
         self.invitation.save()
 
-        response = self.client.get(reverse("invitation", kwargs={'code': self.invitation.code.hex}))
+        response = self.client.get(reverse("invitation", kwargs={'code': self.invitation.code}))
         self.assertEqual(response.status_code, 404)
 
     def test_links_user_when_used(self):
-        invite_path = reverse("invitation", kwargs={'code': self.invitation.code.hex})
+        invite_path = reverse("invitation", kwargs={'code': self.invitation.code})
         data = {"username": "invited_user", "password1": "testpassword", "password2": "testpassword"}
         self.client.post(invite_path, data=data)
 
-        user = User.objects.filter(username="invited_user")[0]
+        user = get_user_model().objects.filter(username="invited_user")[0]
         self.assertIsNotNone(user)
         self.invitation.refresh_from_db()
         self.assertEqual(self.invitation.user_id, user.id)
